91a1b08f95d038f3a6601db29341ac6656e354a0
Victor Wagner <victorwdr@gmail.com>
2020-05-07 13:08:03 -0300

    add covid19-note alert modal

--- src/index.js
+++ src/index.js
@@ -4,3 +4,51 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+function displayNoteModal() {
+  const display = sessionStorage.getItem("persist:@VondME_SPA/display-covid19-note");
+  const modal = document.getElementById("covid19-note");
+
+  if (display) {
+    modal.style.display = display;
+  }
+}
+
+(function(window, document) {
+  const menu = document.getElementById("menu"),
+    aboutItem = document.getElementById("about"),
+    solutionsItem = document.getElementById("solutions"),
+    pricingItem = document.getElementById("pricing"),
+    contactItem = document.getElementById("contact"),
+    blogItem = document.getElementById("post"),
+    ctaItem = document.querySelector(".cta"),
+    WINDOW_CHANGE_EVENT = ("onorientationchange" in window) ? "orientationchange" : "resize";
+
+  function toggleMenu() {
+    menu.classList.toggle("open");
+
+    aboutItem.classList.toggle("it");
+    solutionsItem.classList.toggle("it");
+    pricingItem.classList.toggle("it");
+    contactItem.classList.toggle("it");
+    blogItem.classList.toggle("it");
+    ctaItem.classList.toggle("it");
+
+    document.getElementById("toggle").classList.toggle("x");
+  }
+
+  function closeMenu() {
+    if (menu.classList.contains("open")) {
+      toggleMenu();
+    }
+  }
+
+  document.getElementById("toggle").addEventListener("click", function(e) {
+    toggleMenu();
+    e.preventDefault();
+  });
+
+  displayNoteModal();
+
+  window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
+})(window, window.document);
